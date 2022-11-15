921a8c617c915ea66c322b3d96bdaeb59e0e02d6
zbackya <zbackyaa@gmail.com>
2022-11-16 00:54:44 +0700

    contact, testimoni, profile, report

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -37,7 +37,7 @@
           <div class="col-sm-12">
             <div class="contact-map box">
               <div id="map" class="contact-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.1422937950147!2d-73.98731968482413!3d40.75889497932681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes+Square!5e0!3m2!1ses-419!2sve!4v1510329142834" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1974.6575555611128!2d113.7273972049758!3d-8.17097484936795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sgarda%20adhi%20pratama!5e0!3m2!1sid!2sid!4v1668534795635!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               </div>
             </div>
           </div>
